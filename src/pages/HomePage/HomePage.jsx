69ad63d8d220235d4e1316d20f8b4d186cecc4b6
chris-gallegos <chrisgallegos101@gmail.com>
2022-11-30 14:41:32 -0700

    added home page content

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,22 @@
+import main from '../../assets/images/main.svg'
+import "./HomePage.css"
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
 
   
     return (
     <div className="Profile">
-        <h2>Home</h2>
-        <div>
-            
+        <h2>Job Appster</h2>
+        <div className='section'>
+            <img src={main} alt='main img here' className='' />
+            <h3>Welcome to Job Appster!</h3>
+            <p>Our mission is to make your job search as trackable as possible!</p>
+            <p>We love helping people find their next amazing oppurtunity. We are exitced you are here!</p>
         </div>
+        <button className=""><Link to='/jobs/new' className='btn btn-hero start'>
+            Get Started
+        </Link></button>
     </div>
     )
 }
